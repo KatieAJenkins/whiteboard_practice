373f9cc94b5f0e7071082002be57b6b30f0b5c04
KatieAJenkins <kind2karma@gmail.com>
2017-02-19 20:25:03 -0700

    sum all numbers in array

---
+++ level2.js
@@ -0,0 +1,30 @@
+'use strict'
+//1. Find the sum of all the numbers in an array
+
+//INPUTS: array
+//OUTPUTS: number
+
+//EXAMPLE INPUTS & OUTPUTS w/EDGE CASES:
+//[1,2,3] => 6
+//[2,4,6] => 12
+//[2, 4, 'Yoyoyo'] => 'Array must contain all numbers'
+
+//2 METHODS TO SOLVE:
+//1. For loop
+//2. Reduce method on array
+
+function sumArray(array){
+  var sum = 0;
+  for (var i = 0; i < array.length; i++){
+    if (typeof array[i] !== 'number'){
+      // console.log('Array must contain all numbers');
+      return 'Array must contain all numbers';
+    }
+    sum += array[i];
+  }
+  console.log(sum);
+}
+
+sumArray([1,2,3]);
+sumArray([2,4,6]);
+sumArray([2, 4, 'yoyoyo']);
